a52d5aa2035d5f39c1fa3237d393f0d70ec59ab8
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-11-14 22:46:52 +0200

    rename repo

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/react_search_movies_app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
